d6e1fa3053492e91676878b2a10fe51cc2a64db1
Mirko Rinelli <mirko.rinelli@gmail.com>
2020-11-18 18:38:48 +0100

    Stampare la posizione del cognome inserito

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,7 +4,10 @@ console.log(lastname);
 //Inserire il cognome all'interno dell'arrey di cognomi
 listLastname = ["Bianchi", "Rossi", "Duzioni", "Balsano", "Verdi"]
 listLastname.push(lastname);
+console.log(listLastname);
 //Stampare la lista ordinata alfabeticamente
 listLastname.sort();
 console.log(listLastname);
 // Scrivere la posizione "umana" della lista in cui il nuovo utente si trova
+var findEl = listLastname.indexOf(lastname) +1;
+console.log(findEl);
