48d54d917c0a883fb5d8f35df46c5f9236434ae9
Mirko Rinelli <mirko.rinelli@gmail.com>
2020-11-18 18:03:48 +0100

    Aggiunta nome utente e stampa ordine alfabetico

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,8 @@ var lastname = prompt("Inserisci il tuo cognome");
 console.log(lastname);
 //Inserire il cognome all'interno dell'arrey di cognomi
 listLastname = ["Bianchi", "Rossi", "Duzioni", "Balsano", "Verdi"]
+listLastname.push(lastname);
 //Stampare la lista ordinata alfabeticamente
-
+listLastname.sort();
+console.log(listLastname);
 // Scrivere la posizione "umana" della lista in cui il nuovo utente si trova
