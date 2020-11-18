1847d46020b5eb5680db264d2a62ae035d18d5fd
Mirko Rinelli <mirko.rinelli@gmail.com>
2020-11-18 17:51:25 +0100

    Richiesta cognome ed elenco array inseriti

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+//Chidedere all'utente il cognome
+var lastname = prompt("Inserisci il tuo cognome");
+console.log(lastname);
+//Inserire il cognome all'interno dell'arrey di cognomi
+listLastname = ["Bianchi", "Rossi", "Duzioni", "Balsano", "Verdi"]
+//Stampare la lista ordinata alfabeticamente
+
+// Scrivere la posizione "umana" della lista in cui il nuovo utente si trova
